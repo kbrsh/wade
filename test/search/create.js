0270e7d143a1db382e8ee6164221dee4bb7a17ad
Kabir Shah <simplecooldude1@gmail.com>
2017-07-07 16:38:47 -0700

    add preprocess pipeline tests

--- test/search/create.js
+++ test/search/create.js
@@ -4,7 +4,6 @@ const expect = require("chai").expect;
 describe("Create Search", function() {
   it("should create a search function", function() {
     const search = Wade([]);
-
     expect(search).to.be.a('function');
   });
 });
---
+++ test/search/process.js
@@ -0,0 +1,11 @@
+const Wade = require("../../dist/wade.js");
+const expect = require("chai").expect;
+
+describe("Processing", function() {
+  it("should process data", function() {
+    const data = Wade(["ALL UPPERCASE!!", "This. is wade"]).data;
+
+    expect(data[0]).to.equal('uppercase');
+    expect(data[1]).to.equal('is wade');
+  });
+});
