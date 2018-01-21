b627677ed3022716d8cecdd7e8b467bb78e79d24
Kabir Shah <iamkbrsh@gmail.com>
2018-01-20 17:04:58 -0800

    update tests for new index

--- test/index/index.js
+++ test/index/index.js
@@ -4,43 +4,40 @@ const expect = require("chai").expect;
 describe("Search Index", function() {
   it("should create a search index", function() {
     const index = Wade(["Hey", "Hello", "Branch"]).index;
-    expect(index).to.deep.equal({
-      "h": {
-        "e": {
-          "y": {
-            "data": [
-              1.1666666666666667,
-              0
-            ]
-          },
-          "l": {
-            "l": {
-              "o": {
-                "data": [
-                  1.1666666666666667,
-                  1
-                ]
-              }
-            }
-          }
-        }
-      },
-      "b": {
-        "r": {
-          "a": {
-            "n": {
-              "c": {
-                "h": {
-                  "data": [
-                    1.1666666666666667,
-                    2
-                  ]
-                }
-              }
-            }
-          }
-        }
-      }
-    });
+    expect(index).to.deep.equal([
+      [-97],
+      [
+        [-113],
+        [
+          [-96],
+          [[-109], [[-98], [[-103], [[undefined, 1.1666666666666667, 2]]]]]
+        ]
+      ],
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      [
+        [-100],
+        [
+          [-107],
+          [[-107], [[-110], [[undefined, 1.1666666666666667, 1]]]],
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          [[undefined, 1.1666666666666667, 0]]
+        ]
+      ]
+    ]);
   });
 });
