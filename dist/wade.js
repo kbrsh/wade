cbef43c8f801bb6e8c095b7211a8ddb7f734f20b
Kabir Shah <iamkbrsh@gmail.com>
2018-01-20 17:10:19 -0800

    fix: add punctuation & remove explicit type cast

--- dist/wade.js
+++ dist/wade.js
@@ -17,7 +17,7 @@
     
     var config = {
       stopWords: ["about", "after", "all", "also", "am", "an", "and", "another", "any", "are", "as", "at", "be", "because", "been", "before", "being", "between", "both", "but", "by", "came", "can", "come", "could", "did", "do", "each", "for", "from", "get", "got", "has", "had", "he", "have", "her", "here", "him", "himself", "his", "how", "if", "in", "into", "is", "it", "like", "make", "many", "me", "might", "more", "most", "much", "must", "my", "never", "now", "of", "on", "only", "or", "other", "our", "out", "over", "said", "same", "see", "should", "since", "some", "still", "such", "take", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "up", "very", "was", "way", "we", "well", "were", "what", "where", "which", "while", "who", "with", "would", "you", "your", "a", "i"],
-      punctuationRE: /[!"',.:;?]/g,
+      punctuationRE: /[@!"',.:;?\(\)\[\]]/g,
       processors: [
         function(entry) {
           return entry.toLowerCase();
@@ -57,7 +57,7 @@
           }
     
           if(empty > 0) {
-            output += separator + '@' + empty.toString();
+            output += separator + '@' + empty;
             empty = 0;
             separator = ',';
           }
